5c48190dd69e54bfdd5cb1cde1a598c88aaa3f45
JellyGamez <alexcroitoriu2005@gmail.com>
2023-09-06 17:05:27 +0300

    removed unused files

--- server/api/prisma/get-user.ts
+++ server/api/prisma/get-user.ts
@@ -3,12 +3,8 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    const user = await prisma.user.create({
-        data: {
-            name: 'Alice',
-        },
-    })
-    console.log(user)
+    const users = await prisma.user.findMany()
+    console.log(users)
 }
 
 main()
