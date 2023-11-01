b893af91f78acd1e7b1f324cb9680a759063510e
JellyGamez <minecraft2k2k@gmail.com>
2023-11-01 22:34:35 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,21 +11,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'red-primary':  '#c70404',
-        'red-dark':     '#990000',
-        'red-light':    '#eb1809',
-        'gray-hover':   '#474747',
-        'gray-primary': '#262626',
-        'gray-dark':    '#171717',
-        'gray-light':   '#303030'
+        'red-primary'  : '#c70404',
+        'red-dark'     : '#990000',
+        'red-light'    : '#eb1809',
+        'gray-hover'   : '#474747',
+        'gray-primary' : '#262626',
+        'gray-dark'    : '#171717',
+        'gray-light'   : '#303030'
       },
       maxWidth: {
         '8xl': '90rem',
         '9xl': '110rem',
       },
       fontFamily: {
-        'kanit':  ['Kanit'],
-        'ubuntu': ['Ubuntu']
+        'kanit'  : ['Kanit'],
+        'ubuntu' : ['Ubuntu']
       }
     },
   },
