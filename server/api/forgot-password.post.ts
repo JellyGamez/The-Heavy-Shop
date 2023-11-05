3801c413d53decd6c23bf6b79d2737515a75dfdd
JellyGamez <alexcroitoriu2005@gmail.com>
2023-11-05 14:58:08 +0200

    Update forgot-password.post.ts

--- server/api/forgot-password.post.ts
+++ server/api/forgot-password.post.ts
@@ -2,6 +2,30 @@ import nodemailer from 'nodemailer'
 import { useCompiler } from '#vue-email'
 import { getByEmail } from './prisma/user';
 
+
+
+// const jwt = require('jsonwebtoken')
+// const crypto = require('Crypto')
+// const expireSpan = 3600 * 1000
+// const authSecret = process.env.AUTH_SECRET
+// function generateToken() {
+//     return crypto.randomBytes(30).toString('hex')
+// }
+// function generateTokenExpire() {
+//     return new Date(Date.now() + expireSpan)
+// }
+// function signToken(email: String, token: String) {
+//     return jwt.sign({
+//         email,
+//         token
+//     }, authSecret)
+// }
+// function verifySignedToken(token: String) {
+//     return jwt.verify(token, authSecret)
+// }
+
+
+
 export default defineEventHandler(async (event) => {
     const { email } = await readBody(event)
     if (!email)
@@ -10,11 +34,12 @@ export default defineEventHandler(async (event) => {
             statusMessage: 'The email field is required.'
         })
     
+    // throw error if no user with that email is found
     await getByEmail(email)
 
     const template = await useCompiler('PasswordReset.vue', {
         props: {
-            url: 'https://vuemail.net/',
+            url:  `http://localhost:3000/auth/reset-password?token=${verificationToken}`,
         }
     })
 
