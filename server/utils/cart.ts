6100aed01448d49d809dea05f967e83ccf40d80c
JellyGamez <alexcroitoriu2005@gmail.com>
2024-01-05 00:19:07 +0200

    test

--- server/utils/cart.ts
+++ server/utils/cart.ts
@@ -1,33 +1,33 @@
-// import prisma from './prisma'
+import prisma from './prisma'
 
-// async function getItems(data: any) {
-//     return await prisma.cart.findUnique({
-//         where: {
-//             userId: data.user.id
-//         }
-//     })
-// }
+async function getItems(data: any) {
+    return await prisma.cart.findUnique({
+        where: {
+            userId: data.user.id
+        }
+    })
+}
 
-// async function addItem(data: any) {
-//     return await prisma.cart.update({
-//         where: {
-//             userId: data.user.id
-//         },
-//         data: {
-//             items: { connect: { id: data.item.id } }
-//         },
-//     })
-// }
+async function addItem(data: any) {
+    return await prisma.cart.update({
+        where: {
+            userId: data.user.id
+        },
+        data: {
+            items: { connect: { id: data.item.id } }
+        },
+    })
+}
 
-// async function removeItem(data: any) {
-//     return await prisma.cart.update({
-//         where: {
-//             userId: data.user.id
-//         },
-//         data: {
-//             items: { disconnect: { id: data.item.id } }
-//         },
-//     })
-// }
+async function removeItem(data: any) {
+    return await prisma.cart.update({
+        where: {
+            userId: data.user.id
+        },
+        data: {
+            items: { disconnect: { id: data.item.id } }
+        },
+    })
+}
 
-// export { getItems, addItem, removeItem }
+export { getItems, addItem, removeItem }
